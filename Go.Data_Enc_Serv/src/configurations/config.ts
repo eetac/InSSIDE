ba2a6d2f5c4412949aa8febcd54928a2a4dc2899
krunal <krunalmiracle@gmail.com>
2021-05-11 13:33:21 +0200

    Update config.ts

--- Go.Data_Enc_Serv/src/configurations/config.ts
+++ Go.Data_Enc_Serv/src/configurations/config.ts
@@ -6,20 +6,19 @@ const config:IConfig = {
     URL             :   "http://localhost:3000/api", //Go Data URI
     IV_LENGTH       :   16, // Minimum Length 16 IV
     saltRounds      :   10, // Hash Function Rounds, recommended for security at least 10
-    EMAIL            :   "encryption@local.com",
-    HOSPITAL       :   "admin", // Hospital Name, in our case it is the administrator name for DRM server
-    USER_GODATA_ID    :   "1e054e83-e335-432a-87c5-6f1b5e34d3b9",
+    EMAIL           :   "encryption@local.com",
+    HOSPITAL        :   "admin", // Hospital Name, in our case it is the administrator name for DRM server
+    USER_GODATA_ID  :   "9fd48f7a-58c9-44de-836b-5743c9845b3b",
     PASSWORD        :   "kruskechi1234",
     DOCUMENT_HASH   :   "LNG_REFERENCE_DATA_CATEGORY_DOCUMENT_TYPE_HASHID",
     USER_AGENT      :   "GoData LocalHost Connector 2.5.6",
-    OUTBREAK_ID     :   "2ff2b22e-7f51-4afb-b411-9e29490452a5",
+    OUTBREAK_ID     :   "33227825-a497-46ae-a8b9-8ee30921db3e",
     SENSITIVE_DATA   :   ["firstName","middleName","lastName","documents,number","addresses,phoneNumber"],
     DB              :   {
         URI     : process.env.MONGODB_URI || 'mongodb://localhost:27017/DRM',
         USER    : process.env.MONGODB_USER,
         PASSWORD: process.env.MONGODB_PASSWORD
     },
-    AUTO_ENCRYPTION_TIMER: 0 // Don't put too short, as there is a time required to encrypt and save to DB
-    // Either way, if the time is too short it will only auto encrypt when the previous is complete!
+    AUTO_ENCRYPTION_TIMER: 0 // If the time is too short it will only auto encrypt when the previous is complete!
 };
 module.exports = config;
